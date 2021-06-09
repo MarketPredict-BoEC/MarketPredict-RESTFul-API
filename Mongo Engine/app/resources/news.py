94225a20691deb79d71ecaea5546404b8e21c45d
MarketPredict <61852331+MarketPredict-BoEC@users.noreply.github.com>
2021-06-09 18:38:36 +0430

    Resource files added

--- Mongo Engine/app/resources/news.py
+++ Mongo Engine/app/resources/news.py
@@ -1,29 +1,29 @@
 from flask import request, json, Response
 from flask_restful import Resource
-from flask_apispec import marshal_with
+#from flask_apispec import marshal_with
 from app.response import ResponseAPI
 from app.errors import CustomException
-from app.model.schema.newsModelSchema import NewsModelSchema
 from app.model.newsModel import NewsModel
 import logging as log
 import json
 
-
+# todo: resp.statuscode need to be set. I only set status code in resp Object
 class News(Resource):
     @classmethod
-    #@marshal_with(NewsModelSchema)
+    # @marshal_with(NewsModelSchema)
     def post(cls):
         try:
             data = request.json
             log.info(data)
             newsModel = NewsModel()
-            print(data)
+            #print(data)
             data = newsModel.validatData(data)
             if data:
 
                 if not newsModel.find_by_Link(data['link']):
                     response = newsModel.save_to_DB(data)
                     if response:
+
                         return ResponseAPI.send(status_code=200, message="Inserted successfully", data=str(response))
                     else:
                         return ResponseAPI.send(status_code=404, message="Database Connection Error")
@@ -35,37 +35,37 @@ class News(Resource):
 
         except Exception:
             raise CustomException("user_error", 500, 2201)
-
     @classmethod
     def get(cls):
         try:
             args = request.args
             log.info(args)
+            args.get('link')
+
             if args is None or args == {}:
                 return ResponseAPI.send(status_code=400, message="Please provide request parameters information")
 
-            elif args['link'] is not None:
+            elif args.get('link') is not None:
                 obj1 = NewsModel()
-                response = obj1.find_by_Link(args['link'] )
+                response = obj1.find_by_Link(args['link'])
                 if response:
                     message = "Exist"
                 else:
                     message = "Not Exist"
 
-                return ResponseAPI.send(status_code=200, message=message,data = response)
-
-
-            elif args['category'] == "all":
+                return ResponseAPI.send(status_code=200, message=message, data=response)
+            elif args.get('category') == "all":
                 obj1 = NewsModel()
+                print(args)
                 response = obj1.read()
                 print(response)
-                return ResponseAPI.send(status_code=200, message="Successfully", data=json.dumps(response))
+                return ResponseAPI.send(status_code=200, message="Successfully", data=json.dumps(response, indent=3))
 
-
-            elif args['keywords'] and args['from'] and args['to'] and args['category']:
+            elif args.get('keywords') and args.get('from') and args.get('to') and args.get('category'):
                 log.info("search in mongo")
                 start = int(args['from'])
                 end = int(args['to'])
+
                 if start < end:
                     obj1 = NewsModel()
 
@@ -79,6 +79,17 @@ class News(Resource):
                 else:
                     raise ValueError(
                         "Error: The second date input is earlier than the first one")
+            elif args.get('category').lower() in ["cryptocurrency", "forex", "commodities"] and \
+                    args.get('keywords') and args.get('from') is None:
+                obj1 = NewsModel()
+                response = obj1.find_by_keywords(args.get('category'), args.get('keywords'))
+                return ResponseAPI.send(status_code=200, message="Successfully", data=json.dumps(response, indent=3))
+            elif args.get('category').lower() in ["cryptocurrency", "forex", "commodities"] and args.get(
+                    'keywords') is None:
+                obj1 = NewsModel()
+                response = obj1.find_by_category(args.get('category'))
+                return ResponseAPI.send(status_code=200, message="Successfully", data=json.dumps(response, indent=3))
+
             else:
 
                 return ResponseAPI.send(status_code=422, message="Bad Request Parameters!", data=False)
